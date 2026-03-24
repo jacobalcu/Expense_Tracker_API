f3b7bfc3bc41c6cb10c946fc92cb122cc19f1321
jalcumb <101678531+jacobalcu@users.noreply.github.com>
2026-03-24 12:31:23 -0400

    updated CORS for deployment

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(title="Expense Tracker API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials = True,
     allow_methods=["*"], # allows GET, POST, PUT, DELETE, etc
     allow_headers=["*"], # Allows all headers (like our Authorization header)
